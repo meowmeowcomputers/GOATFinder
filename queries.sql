dc18c9dc701b02a31a874912de77206f2ffe8494
meowmeowcomputers <ryansimonleon@gmail.com>
2017-07-13 15:29:03 -0500

    Query for top starting pitchers by ERA

--- queries.sql
+++ queries.sql
@@ -103,8 +103,34 @@ SELECT fullnames.namefirst, fullnames.namelast, avghr.avghomer
     ON primarypos.playerid=poslist.playerid AND primarypos.position_occurence > poslist.position_occurence
     WHERE primarypos.playerid IS NULL;
 
---Query the top pitchers
+--Query the pitcher names
 SELECT  master.namefirst, master.namelast, pitching.*
   FROM baseball.pitching AS pitching
   JOIN baseball.master AS master
   ON master.playerid = pitching.playerid
+  WHERE pitching.yearid >= 1871
+  AND pitching.yearid <= 2016
+--Query the top starting pitchers within a given date range by average ERA and
+  --innings pitched per year meets ERA championship qualifier
+SELECT eraavg.avgera, pitching.fname, pitching.lname, pitching.playerid, eraavg.avgipouts as average_outs_per_year FROM
+  (SELECT master.namefirst as fname, master.namelast as lname, pitching.*
+    FROM baseball.pitching AS pitching
+    JOIN baseball.master AS master
+    ON master.playerid = pitching.playerid
+ ) as pitching
+  JOIN
+  (SELECT
+    avg(pitching.era) as avgera,
+    pitching.playerid as idavg,
+    avg(pitching.ipouts) as avgipouts
+    FROM baseball.pitching
+    WHERE pitching.yearid >= 2015
+    AND pitching.yearid <= 2016
+    GROUP BY idavg) as eraavg
+  ON eraavg.idavg = pitching.playerid
+  AND eraavg.avgipouts > 486
+  GROUP BY eraavg.avgera, pitching.fname, pitching.lname, pitching.playerid, average_outs_per_year
+  ORDER BY eraavg.avgera ASC;
+--Query to find top bullpen players
+
+UPDATE baseball.pitching SET ip = CAST(ipouts as FLOAT)/3 WHERE ipouts > 0;
